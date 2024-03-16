cd8e1a0ef498c339bc3ea43dee7bd25f9faee546
djmjora <khozakj@gmail.com>
2024-03-16 21:15:45 +0200

    moved from postgres to H2 database.

--- README.md
+++
@@ -1,2 +0,0 @@
-# chat-room
-The chat room 
--- src/main/java/com/chat/room/security/SecurityConfig.java
+++ src/main/java/com/chat/room/security/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.chat.room.security;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
