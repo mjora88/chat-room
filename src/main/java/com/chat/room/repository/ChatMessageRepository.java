166accec235d6c3097fcbde2f2c56e232c632dc8
djmjora <khozakj@gmail.com>
2024-03-17 15:40:22 +0200

    Resolved unused imports and redundant Modifier.

--- src/main/java/com/chat/room/repository/ChatMessageRepository.java
+++ src/main/java/com/chat/room/repository/ChatMessageRepository.java
@@ -5,6 +5,6 @@ import jakarta.annotation.Nullable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    public void deleteById(@Nullable Long id);
+    void deleteById(@Nullable Long id);
 
 }
--- src/main/java/com/chat/room/service/ChatRoomInitializer.java
+++ src/main/java/com/chat/room/service/ChatRoomInitializer.java
@@ -1,6 +1,5 @@
 package com.chat.room.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
