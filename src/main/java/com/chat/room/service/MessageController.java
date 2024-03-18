6868bc8197b2c7eed2f1d9da6c91336748e19fe5
djmjora <khozakj@gmail.com>
2024-03-18 09:08:53 +0200

    Added only authorized users to delete the message.

--- src/main/java/com/chat/room/service/MessageController.java
+++ src/main/java/com/chat/room/service/MessageController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -33,7 +34,7 @@ public class MessageController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
 
         }
-
+    @PreAuthorize("hasRole('ROLE_USER')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteMessages (@PathVariable Long id) {
         if(!messageService.exists(id)) {
